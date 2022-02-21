363c65a0bda725ffc066ffd41117da44dd5f118b
foukonana <anastasiafoudouli@gmail.com>
2022-02-21 20:51:09 +0200

    tfidf keywords periods before and after the financial crisis checkpoint

--- spark-analysis/src/main/scala/auth/dws/bigdata/tasks/Task4TFIDF.scala
+++ spark-analysis/src/main/scala/auth/dws/bigdata/tasks/Task4TFIDF.scala
@@ -6,7 +6,7 @@ import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-object Task3TFIDF {
+object Task4TFIDF {
 
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession
@@ -18,10 +18,24 @@ object Task3TFIDF {
     val start_time = System.nanoTime
 
     // load original csv as DataFrame
-    val original_df = createDataFrame()//.sample(0.01)
+    val original_df = createDataFrame().sample(0.20)
+
+    val quarters_df = original_df
+      .withColumn("sitting_quarter", quarter(column("sitting_date")))
+      .withColumn("sitting_quarter", concat(column("sitting_year"), lit(" Q"), column("sitting_quarter")))
+      // add pro-crisis and after crisis 10 year periods
+      .withColumn("financial_crisis_period",
+        when((column("sitting_quarter") >= "2008 Q4") and (column("sitting_quarter") < "2013 Q4"), "Financial Crisis")
+          .when((column("sitting_quarter") >= "2003 Q4") and (column("sitting_quarter") < "2008 Q4"), "Pre-financial crisis")
+          .otherwise("non_interesting"))
+
+    // keep only the quarters of interest -- 5 years prior to the crisis and 5 years after (main events)
+    val crisis_periods = Seq("Financial Crisis", "Pre-financial crisis")
+    val crisis_quarters_df = quarters_df
+      .filter(column("financial_crisis_period").isin(crisis_periods:_*))
 
     // process speech column
-    val processed_speech_df = processSpeechText(original_df, removeDomainSpecificStopWords = true)
+    val processed_speech_df = processSpeechText(crisis_quarters_df, removeDomainSpecificStopWords = true)
 
     // process dataframe
     val processed_df = processDataFrame(processed_speech_df)
@@ -81,12 +95,9 @@ object Task3TFIDF {
       .withColumn("topN_keywords", get_top_keywords_udf(column("tfidf")))
       .cache()
 
-    // aggregate topN_keywords into a single Array per year and political party
-    val df_per_political_party = df_with_top_keywords.groupBy("political_party", "sitting_year")
-      .agg(flatten(collect_list("topN_keywords")) as "topN_keywords_grouped")
-
-    // aggregate topN_keywords into a single Array per year and member
-    val df_per_member = df_with_top_keywords.groupBy("member_name_with_party", "sitting_year")
+    // add periods of interest -- 5 years prior to the financial crisis and 5 years after
+    val df_per_period = df_with_top_keywords
+      .groupBy("sitting_quarter")
       .agg(flatten(collect_list("topN_keywords")) as "topN_keywords_grouped")
 
     // sort term by frequency and extract top M
@@ -103,32 +114,18 @@ object Task3TFIDF {
     val term_freq_udf = udf(term_freq)
 
     // apply udf to get top keywords in both dataframes
-    val df_per_political_party_final = df_per_political_party
+    val df_per_period_final = df_per_period
       .withColumn("topN_keywords_freq", term_freq_udf(column("topN_keywords_grouped")))
-
-    val df_per_member_final = df_per_member
-      .withColumn("topN_keywords_freq", term_freq_udf(column("topN_keywords_grouped")))
-
-    // print results
-    //df_per_political_party_final.show(false)
-    //df_per_member_final.show(false)
 
     // write results into parquet files
-    val path_to_results = "src/main/scala/auth/dws/bigdata/results/task3"
+    val path_to_results = "src/main/scala/auth/dws/bigdata/results/task4"
 
-    df_per_political_party_final
+    df_per_period_final
       .drop("topN_keywords_grouped")
       .write
       .format("parquet")
       .option("header", "true")
-      .save(s"$path_to_results/keywords_political_party_tfidf.parquet")
-
-    df_per_member_final
-      .drop("topN_keywords_grouped")
-      .write
-      .format("parquet")
-      .option("header", "true")
-      .save(s"$path_to_results/keywords_member_tfidf.parquet")
+      .save(s"$path_to_results/quarterly_keywords.parquet")
 
     val duration = (System.nanoTime - start_time) / 1e9d
     println(s"Execution time was $duration seconds")
